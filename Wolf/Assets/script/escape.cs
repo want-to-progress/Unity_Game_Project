427c1f18ba7681440c1ada851873f96cc8e32677
YUN <32327398+want-to-progress@users.noreply.github.com>
2020-02-29 19:11:31 +0800

    229

---
+++ Wolf/Assets/script/escape.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class escape : MonoBehaviour
+{
+    Image ctrl;
+
+    private void Start()
+    {
+        ctrl = GetComponentInChildren<Image>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+        else if (Input.GetKey(KeyCode.C))
+        {
+            Color temp = ctrl.color;
+            temp.a = 1;
+            ctrl.color = temp;
+        }
+        else
+        {
+            Color temp = ctrl.color;
+            temp.a = 0;
+            ctrl.color = temp;
+        }
+    }
+}
